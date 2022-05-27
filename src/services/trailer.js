cd8d30153693814f6c8354fa63e00705999301ff
typenoob <61347081+typenoob@users.noreply.github.com>
2022-05-27 11:10:31 +0800

    Update trailer.js

--- src/services/trailer.js
+++ src/services/trailer.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const Trailer = titleTrailer => {
   return axios.create({
-    baseURL: `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${process.env.VUE_APP_YOUTUBE_KEY}q=${titleTrailer}&type=video&maxResults=1`
+    baseURL: `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${process.env.VUE_APP_YOUTUBE_KEY}&q=${titleTrailer}&type=video&maxResults=1`
   });
 };
